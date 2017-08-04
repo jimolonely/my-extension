0e7591a10bb18e56acfd205cf2655ef704c0d96f
root <15982406684@163.com>
2017-08-04 08:53:22 +0800

    add tick time

--- js/app.js
+++ js/app.js
@@ -20,19 +20,19 @@ addLogBtn.on('click',function(){
 	if(!log||log==""){
 		return;
 	}
-	logText.val(log+"\n"+logText.val());
+	var now = timeSpan.html();
+	logText.val('# '+now+' #\n'+log+"\n"+logText.val());
 	
-	var title = timeSpan.html()+".log";
-	
-	saveOneLog(title);
+	saveOneLog();
 })
 
-function saveOneLog(title){
+function saveOneLog(){
   if (fileEntry) {
     writeEditorToFile(fileEntry);
   } else {
+	  var title = new Date().Format("yyyy-MM-dd")+".log";
 	  var config = {type: 'saveFile', suggestedName: title};
-    chrome.fileSystem.chooseEntry({type:'saveFile'}, function(theFileEntry){
+    chrome.fileSystem.chooseEntry(config, function(theFileEntry){
 	  console.log(theFileEntry)
 	  fileEntry = theFileEntry;
 	  writeEditorToFile(theFileEntry);
@@ -69,4 +69,31 @@ function writeEditorToFile(theFileEntry) {
 }
 
 //当input focus时清除提示信息
+//TODO
+
+//修改日期
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+//动态时间
+function tick(){
+	var now = new Date().Format("yyyy-MM-dd hh:mm:ss");
+	timeSpan.html(now);
+}
+
+setInterval(tick,1000);
+
+
 
