bd5743a910f84bda4598f04e20a60c9cad91f95d
jackpler <15982406684@163.com>
2017-09-14 16:40:29 +0800

    add cmd h,l,x,c

--- jimo-vim-key/app.js
+++ jimo-vim-key/app.js
@@ -2,6 +2,17 @@
 @Author: https://github.com/jimolonely
 **/
 
+console.log(chrome.extension)
+
+/**
+定义一个content script和background script通信的消息体
+**/
+class Msg{
+	constructor(type,data){
+		this.type = type;
+		this.data = data;
+	}
+}
 
 //G:到页面底部
 $(document).bind('keydown', 'shift+g', function(){
@@ -18,22 +29,48 @@ $(document).bind('keydown', 'k', function(){
 	$(window).scrollTo("-=100px");
 });
 
-//h:往左
+//h:切换到左边一个tab页
 $(document).bind('keydown', 'h', function(){
-	$(window).scrollTo({left:"-=100px"});
+	var msg = new Msg('tab','h');
+	chrome.extension.sendRequest(msg);
 });
 
-//l:往右
+//l:切换到右边一个tab页
 $(document).bind('keydown', 'l', function(){
-	$(window).scrollTo({right:"+=100px"});
+	var msg = new Msg('tab','l');
+	chrome.extension.sendRequest(msg);
+});
+
+//c:打开一个新的tab页
+$(document).bind('keydown', 'c', function(){
+	var msg = new Msg('tab','c');
+	chrome.extension.sendRequest(msg)
+});
+
+//x:关闭当前tab页
+$(document).bind('keydown', 'x', function(){
+	var msg = new Msg('tab','x');
+	chrome.extension.sendRequest(msg)
 });
 
 //esc:
 $(document).bind('keydown', 'esc', function(){
+	doEsc()
+});
+$("input").bind('keydown', 'esc', function(){
+	doEsc();
+});
+$("textarea").bind('keydown', 'esc', function(){
+	doEsc();
+});
+
+function doEsc(){
 	console.log('esc')
 	//隐藏cmdDiv
 	$('#jimo-cmd-div').hide()
-});
+	$("input").blur();
+	$("textarea").blur();
+}
 
 /**
 :(冒号)=shift+;
@@ -45,8 +82,8 @@ $(document).bind('keydown', 'shift+;', function(){
 	console.log('shift+;')
 	//新增div元素包括输入框，使用绝对定位在左下角
 	if(!createdCmdDiv){
-		var cmdDiv = '<div id="jimo-cmd-div" style="width:150px;height:25px;background:#000;position:fixed;bottom:0px;left:0px;">'
-		+'<b>:</b><input id="jimo-cmd-input" type="text" style="width:90%;border:0px solid #000;background:#000;"/>'
+		var cmdDiv = '<div id="jimo-cmd-div" style="width:150px;height:25px;background:#000;position:fixed;bottom:0px;left:0px;color:#ddd;z-index:10000000;">'
+		+'<b>:</b><input id="jimo-cmd-input" type="text" style="width:90%;border:0px solid #000;background:#000;color:#ddd"/>'
 		+'</div>'
 		$("body").append(cmdDiv);		
 		createdCmdDiv = true;
@@ -57,7 +94,7 @@ $(document).bind('keydown', 'shift+;', function(){
 	clearCmdIput = true;//按下shift+;时会把:写入input
 });
 
-//监听cmdInput的按键命令
+//监听cmdInput的按键命令,因为是动态生成的，所以单独处理
 $("body").on('keydown','#jimo-cmd-input', function(e){
 	switch(e.keyCode){
 	case 13://enter
@@ -88,7 +125,7 @@ $(document).keydown(function(e){
 			gPressed = false;
 		}else{
 			gPressed = true;
-			beginTime = new Date().getTime();
+			beginTime = Date.now();
 		}
 	} 
 	
@@ -96,7 +133,7 @@ $(document).keydown(function(e){
 
 //判断第一次与第二次按键的间隔是否在1s内
 function inTime(){
-	var end = new Date().getTime();
+	var end = Date.now();
 	if(end-beginTime>1000){
 		return false;
 	}
